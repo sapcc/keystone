d2c564dca5a3ffc185203a986aa2924326ebee0e
Maurice Escher <maurice.escher@sap.com>
2020-02-06 17:24:12 +0100

    ccloud: add possibility to set default tag(s)
    configuration option to set tags that get added to newly created projects
    
    Change-Id: Icac8d54506082816bdaeacb73853a20b49735c16

--- keystone/conf/default.py
+++ keystone/conf/default.py
@@ -104,6 +104,17 @@ Default `publisher_id` for outgoing notifications. If left undefined, Keystone
 will default to using the server's host name.
 """))
 
+default_tag = cfg.MultiStrOpt(
+    'default_tag',
+    default=[],
+    help=utils.fmt("""
+Default `tag`(s) for newly created projects. If left undefined, Keystone will
+create projects without an initial tag. This field can be set multiple times in
+order to set multiple default tags, for example:
+default_tag=tag_0
+default_tag=tag_1
+"""))
+
 notification_format = cfg.StrOpt(
     'notification_format',
     default='cadf',
@@ -147,6 +158,7 @@ ALL_OPTS = [
     strict_password_check,
     insecure_debug,
     default_publisher_id,
+    default_tag,
     notification_format,
     notification_opt_out,
 ]
